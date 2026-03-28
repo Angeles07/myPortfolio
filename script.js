fd4e02a2ed5291a4e34e4b1669eb481c4ba3a36d
angeles07 <angelesmuntasir@gmail.com>
2026-03-28 21:09:22 +0800

    with form

--- script.js
+++ script.js
@@ -99,3 +99,23 @@ project.classList.add('hide')
 
 })
 })
+
+document.querySelector("form").addEventListener("submit", async function(e){
+  e.preventDefault()
+
+  const form = e.target
+  const data = new FormData(form)
+
+  const response = await fetch(form.action, {
+    method: form.method,
+    body: data,
+    headers: { 'Accept': 'application/json' }
+  })
+
+  if(response.ok){
+    alert("Message sent successfully!")
+    form.reset()
+  } else {
+    alert("Something went wrong.")
+  }
+})
